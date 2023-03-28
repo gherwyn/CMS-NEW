80f9b4d7bd4c9dc472bdc31fcf85d81cfaa4de15
gherwyn <bgherwyn@gmail.com>
2023-03-28 10:29:13 +0800

    Add New and HTTPResponseRedirect to List

--- cmsmain/views.py
+++ cmsmain/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Projects
+from django.core.exceptions import ObjectDoesNotExist
+
 # Create your views here.
 
 def index(request):
@@ -11,14 +13,22 @@ def index(request):
 def add(request):
     return render(request, 'projects/add.html')
 
-def processAdd(request):
+def processadd(request):
     fname = request.POST.get('fname')
-    fname = request.POST.get('lname')
-    fname = request.POST.get('email')
-    fname = request.POST.get('position')
+    lname = request.POST.get('lname')
+    email = request.POST.get('email')
+    position = request.POST.get('position')
     if request.FILES.get('image'):
-        user_pic = request.FILE.get('image')
+        user_pic = request.FILES.get('image')
     else:
         user_pic = 'profile/image.jpg'
     try:
-        n = 
+        n = Projects.objects.get(user_email=email)
+        # number already exists
+        return render(request, 'projects/add.html', {
+            'error_message': "Duplicated email: " + email
+        })
+    except ObjectDoesNotExist:
+        user = Projects.objects.create(user_email=email, user_fname=fname, user_lname=lname, user_position=position, user_image=user_pic)
+        user.save()
+        return HttpResponseRedirect('/cmsmain')
