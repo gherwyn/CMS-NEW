3cbdb4de99e501334f006d89161b4346cbf6d2fb
gherwyn <bgherwyn@gmail.com>
2023-03-29 15:13:31 +0800

    View Details and Pagination
    View Details and Pagination

--- cmsmain/urls.py
+++ cmsmain/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.add, name='add'),
     path('processadd', views.processadd, name='processadd'),
+    path('<int:projects_id>/detail/', views.detail, name='detail'),
+    path('<int:projects_id>/delete/', views.delete, name='delete'),
+    path('<int:projects_id>/edit/', views.edit, name='edit'),
+    path('<int:projects_id>/processedit/', views.processedit, name='processedit'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- cmsmain/views.py
+++ cmsmain/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import Projects
+from django.core.paginator import Paginator
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
 def index(request):
-    project_list = Projects.objects.order_by('pub_date')[:5]
-    context = {'project_list': project_list}
-    return render(request, 'projects/index.html',context)
+    project_list = Projects.objects.all().order_by('-pub_date')
+    
+    paginator = Paginator(project_list, 5)
+
+    page_number = request.GET.get('page')
+
+    project_list = paginator.get_page(page_number)
+
+    return render(request, 'projects/index.html', { 'page_obj': project_list })
 
 def add(request):
     return render(request, 'projects/add.html')
@@ -32,3 +39,45 @@ def processadd(request):
         user = Projects.objects.create(user_email=email, user_fname=fname, user_lname=lname, user_position=position, user_image=user_pic)
         user.save()
         return HttpResponseRedirect('/cmsmain')
+
+def detail(request, projects_id):
+    try:
+        projects = Projects.objects.get(pk=projects_id)
+    except Projects.DoesNotExist:
+        raise Http404("Profile does not exist")
+    return render(request, 'projects/detail.html', {'projects': projects})
+
+def delete(request, projects_id):
+    Projects.objects.filter(id=projects_id).delete()
+    return HttpResponseRedirect('/cmsmain')
+
+def edit(request, projects_id):
+    try:
+        projects = Projects.objects.get(pk=projects_id)
+    except Projects.DoesNotExist:
+        raise Http404("Profile does not exist")
+    return render(request, 'projects/edit.html', {'projects': projects})
+
+def processedit(request, projects_id):
+    user = get_object_or_404(Projects, pk=projects_id)
+    projects_pic = request.FILES.get('image')
+    try:
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        position = request.POST.get('position')
+    except (KeyError, Projects.DoesNotExist):
+        return render(request, 'cmsmain/detail.html', {
+            'projects': Projects,
+            'error_message': "Problem updating record"
+        })
+    else:
+        projects_info = Projects.objects.get(id=projects_id)
+        projects_info.user_fname = fname
+        projects_info.user_lname = lname
+        projects_info.user_email = email
+        projects_info.user_position = position
+        if projects_pic:
+            projects_info.user_image = projects_pic
+        projects_info.save()
+        return HttpResponseRedirect(reverse('cmsmain:detail' , args=(projects_id,)))
